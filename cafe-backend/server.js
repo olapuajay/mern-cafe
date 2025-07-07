1238075f42e12ca4b6b1563c35ed3d699286134e
Olapu Ajay <olapuajay@gmail.com>
2025-07-07 16:38:50 +0530

    Initail commit

---
+++ cafe-backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRoute.js";
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/lpu").then(() => {
+  app.listen(8000, () => {
+    console.log("server started on port 8000");
+    console.log("connected to db");
+  });
+});
+
+app.use("/api/users", userRouter);
